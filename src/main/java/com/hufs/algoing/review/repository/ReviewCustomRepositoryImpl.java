ffa6463a72ce2e1dca607db1cb8be75232d8d496
songhee <songhee9327@gmail.com>
2025-05-12 21:11:48 +0900

    feat : 리뷰 로직 수정

--- src/main/java/com/hufs/algoing/review/repository/ReviewCustomRepositoryImpl.java
+++ src/main/java/com/hufs/algoing/review/repository/ReviewCustomRepositoryImpl.java
@@ -4,6 +4,9 @@ import com.hufs.algoing.review.entity.Review;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 import static com.hufs.algoing.review.entity.QReview.review;
 
 @Repository
@@ -11,14 +14,21 @@ import static com.hufs.algoing.review.entity.QReview.review;
 public class ReviewCustomRepositoryImpl implements ReviewCustomRepository {
     private final JPAQueryFactory jpaQueryFactory;
 
-    // 가장 최신 리뷰
     @Override
-    public Review getReview(Long userId, Long problemNum) {
+    public Review getRecentReview(Long userId, Long problemNum) {
         return jpaQueryFactory
                 .selectFrom(review)
                 .where(review.user.userId.eq(userId))
                 .where(review.problemNum.eq(problemNum))
                 .orderBy(review.createdAt.desc())
                 .fetchFirst();
+    }
+
+    @Override
+    public List<Review> getUserReviews(Long userId) {
+        return jpaQueryFactory
+                .selectFrom(review)
+                .where(review.user.userId.eq(userId))
+                .fetch();
     }
 }
